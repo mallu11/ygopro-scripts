83f5a69c4ccd5033434343fa2d1b217381adc278
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-09-18 17:35:06 +0800

    fix アンチエイリアン

--- c43583400.lua
+++ c43583400.lua
@@ -46,11 +46,14 @@ function c43583400.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
 end
 function c43583400.sumop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-	local g=Duel.SelectMatchingCard(tp,c43583400.sumfilter,tp,LOCATION_HAND,0,1,1,nil)
-	local tc=g:GetFirst()
-	if tc then
-		Duel.Summon(tp,tc,true,nil)
+	local ph=Duel.GetCurrentPhase()
+    	if ph~=PHASE_DAMAGE and ph~=PHASE_DAMAGE_CAL then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+		local g=Duel.SelectMatchingCard(tp,c43583400.sumfilter,tp,LOCATION_HAND,0,1,1,nil)
+		local tc=g:GetFirst()
+		if tc then
+			Duel.Summon(tp,tc,true,nil)
+		end
 	end
 end
 function c43583400.drcon(e,tp,eg,ep,ev,re,r,rp)
